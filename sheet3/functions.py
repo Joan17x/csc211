d66663292923a6bf17f9e062c8ba84a71f047409
Joan Alexander <joanalexander2x@icloud.com>
2023-07-10 16:59:23 -0400

    challenge 4

---
+++ sheet3/functions.py
@@ -0,0 +1,10 @@
+def moan():
+    print("Python is useless!")
+    print("And so are these worksheets.")
+
+def mult(x):
+    result = x*x
+    print("%d * %d is %d" %(x,x,result))
+moan()
+x = int(input("Enter the number you want squared: "))
+mult(x)

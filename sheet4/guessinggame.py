fdb43255600d5e34c48a422455a56969464e9301
Joan Alexander <joanalexander2x@icloud.com>
2023-07-13 16:55:22 -0400

    challenge 6

--- sheet4/guessinggame.py
+++ sheet4/guessinggame.py
@@ -1,20 +1,29 @@
 from random import randint
-number = randint(1, 100)
-question = "What's your guess?"
-print("I've thought of a number between 1 and 100. Try to guess it.")
-guess = 0
-guesses = 0
+from gasp.utils import read_yesorno
 
 while True:
-    guess = (int(input(question)))
-    if guess < number:
-        print("That's too low.")
-        guesses = guesses +1
-    if guess > number:
-        print("That's too high.")
-        guesses = guesses +1
-    if guess == number:
-        print("That was my number. Well done!")
-        guesses = guesses +1
-        print("You took " + str(guesses) + " guesses.")
+    question = "Make a guess: "
+    nquestion = "What would you like the range to be? "
+    n = (int(input(nquestion)))
+    number = randint(1, n)
+    print("I've thought of a number between 1 and " + str(n) + ". Try to guess it.")
+    guess = 0
+    guesses = 0
+    while True:
+        guess = (int(input(question)))
+        if guess < number:
+            print("That's too low.")
+            guesses = guesses +1
+        if guess > number:
+            print("That's too high.")
+            guesses = guesses +1
+        if guess == number:
+            print("That was my number. Well done!")
+            guesses = guesses +1
+            print("You took " + str(guesses) + " guesses.")
+            break
+    if read_yesorno("Would you like another game?"):
+        print("Bet")
+    else:
+        print("Okay, Bye!")
         break

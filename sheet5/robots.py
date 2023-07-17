487fe7f77182b07e37a86d60bdef38e016eefdd0
Joan Alexander <joanalexander2x@icloud.com>
2023-07-17 11:05:42 -0400

    challenge 4

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -1,13 +1,15 @@
 from gasp import *
 from random import randint
-player_x = randint(0, 60)
+player_x = randint(0, 63)
 player_y = randint(0, 47)
 
 def place_player():
     print("Here I am!")
-    Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+    c = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
 
 def move_player():
+    global c
+    c = move_to(c, (player_x, player_y))
     print("I'm moving...")
     update_when('key_pressed')
 
